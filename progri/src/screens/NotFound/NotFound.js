0576fc6f8eb0682969396014bcb3e21ce1a43c49
marucalliera <mcalliera@udesa.edu.ar>
2022-09-08 13:04:56 -0300

    termino

---
+++ progri/src/screens/NotFound/NotFound.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+
+function NotFound() {
+    return (
+        <React.Fragment>
+            <h1>Estas en cualquier ruta</h1>
+
+        </React.Fragment>
+
+    );
+}
+
+export default NotFound;
