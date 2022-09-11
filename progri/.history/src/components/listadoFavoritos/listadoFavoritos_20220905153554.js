5f51dbc99231cfab54a04eab893a3ddf16d2187d
marucalliera <mcalliera@udesa.edu.ar>
2022-09-11 10:04:22 -0300

    .

--- progri/.history/src/components/listadoFavoritos/listadoFavoritos_20220905153554.js
+++ progri/.history/src/components/listadoFavoritos/listadoFavoritos_20220905153554.js
@@ -3,43 +3,6 @@ import UnaPeliculaListado from '../UnaPeliculaListado/UnaPeliculaListado';
 
 
 let apikey = '73bb2c441012fc5a8247a1d5cb33d69a'
-class ListadoFavs extends Component{  
-    constructor(props){
-        super(props);
-        this.state = {
-            datos: [],
-            input: ''
-        }
-    }
-    componentDidMount () {
-        let listaFavs = []
-        if(this.props.funcionalidades.tipo === 'peliculas') {listaFavs = JSON.parse(localStorage.getItem('favoritos'));}
-        listaFavs.map((id)=> {
-            fetch(this.props.funcionalidades.tipo === 'peliculas' ? `https://api.themoviedb.org/3/movie/${id}?api_key=${apikey}`:'MARU')
-            .then(response=>response.json())
-            .then(data=> {
-                let listaDatos = this.state.datos;
-                listaDatos.push(data)
-                this.setState({datos: listaDatos})
-            })
-            .catch(error=>console.log('El error fue: ' + error))
-        })
-    }
 
-    render () {
-        return (
-            <React.Fragment>
-                <h1>{this.props.funcionalidades.tipo === 'peliculas' ? 'Peliculas Favoritas' : 'Series favoritas'}</h1>
-                <p>{this.props.funcionalidades.formFiltro ? 'aca va el input y el form' : ''}</p>           
-                <section className='card-container'>
-                    {this.state.datos === [] ? <h3>Cargando ...</h3> : 
-                    this.state.datos.map((unPersonaje, idx) => <UnaPeliculaListado props={unPersonaje} key={idx} />)
-                    }
-                </section>
-            </React.Fragment>
-        )
-
-    }
-}
 
 export default ListadoFavs;
