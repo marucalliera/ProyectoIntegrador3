5e49ba647987ed4bffda8e151c6b9dff75b0b15c
marucalliera <mcalliera@udesa.edu.ar>
2022-09-14 18:30:30 -0300

    Cambios

--- src/screens/detallePel/detallePel.js
+++ src/screens/detallePel/detallePel.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import UnaPeliculaListado from '../../components/UnaPeliculaListado/UnaPeliculaListado';
+import  "./detallePel.css";
 
 
 let apikey = '73bb2c441012fc5a8247a1d5cb33d69a'
