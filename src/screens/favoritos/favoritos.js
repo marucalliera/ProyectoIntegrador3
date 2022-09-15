ed301159b40aa7eeee0e7ac4247ce8dea7a81151
marucalliera <mcalliera@udesa.edu.ar>
2022-09-15 12:58:28 -0300

    borre Cartel

--- src/screens/favoritos/favoritos.js
+++ src/screens/favoritos/favoritos.js
@@ -5,8 +5,7 @@ import ListadoFav from "../../components/listadoFavoritos/listadoFavoritos";
 function Favoritos() {
     return (
         <React.Fragment>
-            {/* formulario de busqueda */}
-            
+
             <ListadoFav  funcionalidades={{tipo:'peliculas',formFiltro: true}} />
 
         </React.Fragment>
