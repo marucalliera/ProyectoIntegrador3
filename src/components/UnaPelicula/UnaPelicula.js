ec0dd1853f3da8ffe89a423369063ffd99bff996
marucalliera <mcalliera@udesa.edu.ar>
2022-09-17 21:52:55 -0300

    mas cambios en los css

--- src/components/UnaPelicula/UnaPelicula.js
+++ src/components/UnaPelicula/UnaPelicula.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import {Link} from 'react-router-dom';
-import Tarjeta from '../Tarjeta/Tarjeta';
-import './UnaPelicula.css'
+
+
 
 
 class UnaPelicula extends Component{
